c485fe7d440637a04884010b2857d4b1672aa107
BJ <byeongjin.kim@wisecommerce.io>
2022-03-03 12:19:06 +0900

    todo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+import Todo from './Todo.vue'
 
 defineProps({
   msg: String
@@ -9,28 +10,10 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
+  <Todo>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  </Todo>
 </template>
 
 <style scoped>
---
+++ src/components/Todo.vue
@@ -0,0 +1,49 @@
+<script setup>
+import {ref} from 'vue'
+
+const add_input = ref('');
+const todos = ref([
+]);
+
+function doAdd() {
+//  input_message.value = input_message.value.split('').reverse().join('')
+  var input_data = add_input.value;
+
+  if (input_data === '') {
+    return;
+  }
+  todos.value.push({comment: input_data});
+  add_input.value = '';
+}
+
+function doDelete(idx) {
+  todos.value.splice(idx, 1);
+}
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model="add_input">
+    <button v-on:click="doAdd">추가</button>
+  </div>
+
+  <div>
+    <ol>
+      <li class="todo_list" v-for="(value, index) in todos" :key="index">
+        {{ value.comment }}
+        <button v-on:click="doDelete(index)">완료</button>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
